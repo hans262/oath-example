ceed27e7a70fe66d799b7ec9485d491f99c2c936
ouna <771565119@qq.com>
2019-08-06 21:06:44 +0800

    dqwdwq

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-// Fill in your client ID and client secret that you obtained
-// while registering the application
+/**
+ * 注册获取 客户端id 客户端密钥
+ */
 const clientID = 'aa170974839e19dc02c9'
 const clientSecret = '75cb932e1d57f21c2cc0e6576f4417c493f62c7a'
 
-const Koa = require('koa')
-const path = require('path')
-const serve = require('koa-static')
-const route = require('koa-route')
+const { createServer } = require('http')
+const { parse } = require('url')
 const axios = require('axios')
+const server = createServer(handler)
+server.listen(8080, () => {
+  console.log('8080 on port')
+})
 
-const app = new Koa()
+function handler(req, res) {
+  const { pathname, query } = parse(req.url, true)
+  req.__query = query
+  switch (pathname) {
+    case '/favicon.ico':
+      return res.end()
+    case '/':
+      return home(req, res)
+    case '/oauth/redirect':
+      return oauth(req, res)
+    default:
+      return res.end('404')
+  }
+}
 
-const main = serve(path.join(__dirname + '/public'))
-
-const oauth = async ctx => {
-  const requestToken = ctx.request.query.code
-  console.log('authorization code:', requestToken)
-
+async function oauth(req, res) {
+  const requestToken = req.__query.code
+  //fetch accessToken
   const tokenResponse = await axios({
     method: 'post',
     url: 'https://github.com/login/oauth/access_token?' +
@@ -27,11 +40,9 @@ const oauth = async ctx => {
       accept: 'application/json'
     }
   })
-
   const accessToken = tokenResponse.data.access_token
-  console.log(`access token: ${accessToken}`)
-
-  const result = await axios({
+  //fetch user msg
+  const userResponse = await axios({
     method: 'get',
     url: `https://api.github.com/user`,
     headers: {
@@ -39,11 +50,15 @@ const oauth = async ctx => {
       Authorization: `token ${accessToken}`
     }
   })
-  const name = result.data.login
-  ctx.response.redirect(`/welcome.html?name=${name}`)
+  const user = userResponse.data
+  const { login: name } = user
+  res.end(`Welcome ${name}!`)
 }
 
-app.use(main)
-app.use(route.get('/oauth/redirect', oauth))
+function home(req, res) {
+  const authorize_uri = 'https://github.com/login/oauth/authorize'
+  const redirect_uri = 'http://localhost:8080/oauth/redirect'
 
-app.listen(8080)
+  const href = `${authorize_uri}?client_id=${clientID}&redirect_uri=${redirect_uri}`
+  res.end(`<a id="login" href="${href}">Login with GitHub</a>`)
+}
